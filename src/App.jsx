68c1935bfd1b2ea0d4dc3d7c10b78bb251829d1e
Himanshu Ahirwar <hiimanshu028@gmail.com>
2024-11-20 05:16:46 +0530

    v0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import BlurBackground from "./components/BlurBackground"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <>
     <BlurBackground />
+    <Navbar />
+    <main className="antialiased overflow-x-hidden max-w-7xl mx-auto relative z-10">
+      <Navbar />
+      <Hero />
+    </main>
     </>
   )
 }
